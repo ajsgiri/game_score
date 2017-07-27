b554ec68a9002d17e1413ef065fe6ec210778525
Ajay Giri <asgiri@ucsd.edu>
2017-07-27 15:58:17 -0700

    working progress

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var config = function(defaults, custom) {
+var mergeObjects = function(defaults, custom) {
   return Object.assign( {}, defaults, custom);
 };
 
@@ -11,9 +11,10 @@ module.exports = {
       difficulty: "easy", // easy, normal, difficult
       userPlay: false, // game is ongoing
       gameComplete: false,  // can't self reference using defaults.lives
+      highScore: 0 // integer
     };
-
-    this.game = config(defaults, overrides);
+    this.game = mergeObjects(defaults, overrides);
+    this.gameConfig = mergeObjects(defaults, overrides);
     return this.game;
   },
 
@@ -21,19 +22,15 @@ module.exports = {
     var defaults = {
       name: "Sneaky Snek", // any string
       health: 100, // percentage
-      currentScore: 0, // integer
-      highScore: 0 // integer
+      currentScore: 0 // integer
     };
-
-    this.user = config(defaults, overrides);
+    this.user = mergeObjects(defaults, overrides);
+    this.userConfig = mergeObjects(defaults, overrides);
     return this.user;
   },
 
   incrementScore: function() {
       this.user.currentScore+=this.game.scoreIncrement;
-      if (this.user.currentScore >= this.user.highScore) {
-        this.user.highScore = this.user.currentScore;
-      }
       return this.user;
   },
 
@@ -45,15 +42,32 @@ module.exports = {
       return this.user;
   },
 
-  loseLife: function() {
+  loseLife: function(func) {
     this.game.lives-=1;
+    if (this.user.currentScore >= this.game.highScore) {
+      this.game.highScore = this.user.currentScore;
+    }
     if (this.game.lives <= 0) {
-      this.gameOver();
+      // reset game config/user stuff THEN execute gameOver
+      this.resetGame();
+      this.gameOver(func);
     }
     return this.game;
   },
 
-  gameOver: function() {
-    return this.game;
+  resetGame: function() {
+    this.game = mergeObjects(this.gameConfig, { highScore: this.game.highScore });
+    this.user = mergeObjects(this.userConfig, {});
+    //this.user = mergeObjects(this.userConfig, { currentScore: 0});
+    return {
+      game: this.game,
+      user: this.user
+    }
   },
+
+  gameOver: function(func) {
+    if (func) {
+      func();
+    }
+  }
 };
